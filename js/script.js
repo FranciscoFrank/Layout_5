afb8bd6d64ea272446a5c10c571b479eb2e6adaf
Francisco <kolia.boychak@gmail.com>
2024-02-07 18:16:03 +0200

    Created the structure and styles for the reviews block

--- js/script.js
+++ js/script.js
@@ -45,3 +45,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: false,
+    infinity: true,
+    dots: true,
+  });
+});
